d8672751387cef14de82a784be426711b9f47cdb
PihooGokhle <pihoogokhle@gmail.com>
2025-02-28 15:03:43 +0530

    add ques1.c file

---
+++ ques1.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+
+int main(){
+
+    char c = 'P';
+    printf("Size of character constant 'P' is %lu\n", sizeof('P'));
+    printf("Size of character constant 'c' is %lu\n", sizeof(c));
+
+
+    return 0;
+}
---
+++ ques2.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+
+int main(){
+
+    float f = 4.5;
+    printf("Size of real constant 'f' is %lu", sizeof(f));
+
+    return 0; 
+}
